4bac36fbe8df660f8ec82943f659afbca9fef06b
Aleksandr Ustimenko <yst_al@inbox.ru>
2021-10-30 19:40:19 +0300

    refactor: change scrypt.js sctructure

--- scrypt.js
+++ scrypt.js
@@ -1,28 +1,18 @@
 const calcNumbers = (operation, firstNumber, secondNumber) => {
-	if (operation == ("Addition")) {
-		return firstNumber + secondNumber;
-	} else if (operation == ("Subtraction")) {
-		return firstNumber - secondNumber;
-	} else if (operation == ("Multiplication")) {
-		return firstNumber * secondNumber;
-	} else if (operation == ("Division")) {
-		return firstNumber / secondNumber 
-	} else if (operation == ("Exponentiation")) {
-		return firstNumber ** secondNumber;
-	} else if (operation == ("Modulus")) {
-		return firstNumber % secondNumber;
-	} else if (operation == undefined 
-		|| firstNumber== undefined 
-		|| secondNumber == undefined 
-		|| isNaN(secondNumber)
-		|| isNaN(firstNumber)) {
+	if (!operation || !firstNumber || !secondNumber || isNaN(secondNumber) || isNaN(firstNumber)) {
 		return "Error";
 	} else {
-		return "Unknown operation";
-	}
-
+		switch (operation) {
+			case "+": return firstNumber + secondNumber;
+			case "-": return firstNumber - secondNumber;
+			case "*": return firstNumber * secondNumber;
+			case "/": return firstNumber / secondNumber;
+			case "**": return firstNumber ** secondNumber;
+			case "%": return firstNumber % secondNumber;
+			default: return "Unknown operation";
+		}
+	}	
 }
-
-console.log(calcNumbers("Exponentiation",3,4));
+console.log(calcNumbers("qawefqwef", 4, 3));
 
 
