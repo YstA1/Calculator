f82a400eceda2fd64a82d902ed74fd254fbce834
Aleksandr Ustimenko <yst_al@inbox.ru>
2021-10-31 13:30:48 +0300

    refactor: change scrypt.js sctructure

--- scrypt.js
+++ scrypt.js
@@ -1,18 +1,23 @@
-const calcNumbers = (operation, firstNumber, secondNumber) => {
-	if (!operation || !firstNumber || !secondNumber || isNaN(secondNumber) || isNaN(firstNumber)) {
-		return "Error";
-	} else {
-		switch (operation) {
-			case "+": return firstNumber + secondNumber;
-			case "-": return firstNumber - secondNumber;
-			case "*": return firstNumber * secondNumber;
-			case "/": return firstNumber / secondNumber;
-			case "**": return firstNumber ** secondNumber;
-			case "%": return firstNumber % secondNumber;
-			default: return "Unknown operation";
-		}
-	}	
+function checkData (operation, firstNumber, secondNumber) {
+	if (!operation || !firstNumber || !secondNumber || isNaN(secondNumber) || isNaN(firstNumber)) return "Error";
+
+	let result = calcNumbers(operation, firstNumber, secondNumber);
+	
+	console.log(result);
 }
-console.log(calcNumbers("qawefqwef", 4, 3));
+
+function calcNumbers (operation, firstNumber, secondNumber) {
+	switch (operation) {
+		case "+": return firstNumber + secondNumber;
+		case "-": return firstNumber - secondNumber;
+		case "*": return firstNumber * secondNumber;
+		case "/": return firstNumber / secondNumber;
+		case "**": return firstNumber ** secondNumber;
+		case "%": return firstNumber % secondNumber;
+		default: return "Unknown operation";
+	}
+}
+
+checkData( "*", 4, 3);
 
 
