7aefe7c83ebaa49967a5ce3af9d688deb6ac076a
Aleksandr Ustimenko <yst_al@inbox.ru>
2021-11-04 11:24:44 +0300

    :feat: add operations object

--- scrypt.js
+++ scrypt.js
@@ -1,23 +1,32 @@
 function checkData (operation, firstNumber, secondNumber) {
 	if (!operation || !firstNumber || !secondNumber || isNaN(secondNumber) || isNaN(firstNumber)) return "Error";
-
-	let result = calcNumbers(operation, firstNumber, secondNumber);
-	
-	console.log(result);
+    let result = calcNumbers(operation, firstNumber, secondNumber);
+	return result;
 }
 
 function calcNumbers (operation, firstNumber, secondNumber) {
+   let operations = {
+        sum: firstNumber + secondNumber,
+        sub: firstNumber - secondNumber,
+        mult: firstNumber * secondNumber,
+        div: firstNumber / secondNumber,
+    }
+    
 	switch (operation) {
-		case "+": return firstNumber + secondNumber;
-		case "-": return firstNumber - secondNumber;
-		case "*": return firstNumber * secondNumber;
-		case "/": return firstNumber / secondNumber;
-		case "**": return firstNumber ** secondNumber;
-		case "%": return firstNumber % secondNumber;
+		case "sum": return operations.sum;
+		case "sub": return operations.sub;
+		case "mult": return operations.mult;
+		case "div": return operations.div;
 		default: return "Unknown operation";
 	}
 }
 
-checkData( "*", 4, 3);
+console.log(checkData("sum", 6, 3));
+console.log(checkData("sub", 6, 3));
+console.log(checkData("mult", 6, 3));
+console.log(checkData("div", 6, 3));
+console.log(checkData("sm", 6, 3));
+console.log(checkData("", 6, 3));
+console.log(checkData("sum", 6));
 
 
